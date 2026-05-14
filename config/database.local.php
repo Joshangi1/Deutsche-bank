fad80f413bea8b6853c8b596cfe27ea0f51fbb72
Joshangi1 <67809599+Joshangi1@users.noreply.github.com>
2026-05-14 14:50:55 -0700

    Add files via upload

---
+++ config/database.local.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'u626255957_Deutsche_Bank');
+define('DB_USER', 'u626255957_Deutsche_Bank');
+define('DB_PASS', 'Joshangi1@@');
+define('DB_CHARSET', 'utf8mb4');
